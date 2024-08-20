8132654e34353d7c767583739f8bb469efb1bdcd
Kazerlelutin <b@bouteiller.contact>
2024-08-20 23:10:20 +0200

    sort

--- api/club/search.js
+++ api/club/search.js
@@ -4,33 +4,37 @@ import clubs from '../../data/clubs.json';
 export async function clubSearchClub(_req, search) {
 	return new Response(
 		JSON.stringify(
-			clubs.filter((club) => {
-				const values = Object.values(club)
-					.filter((v) => typeof v === 'string' || (typeof v === 'number' && v !== 0))
-					.map((v) => v.toString().toLocaleLowerCase())
-					.filter((v) => v.length > 1);
+			clubs
+				.filter((club) => {
+					const values = Object.values(club)
+						.filter((v) => typeof v === 'string' || (typeof v === 'number' && v !== 0))
+						.map((v) => v.toString().toLocaleLowerCase())
+						.filter((v) => v.length > 1);
 
-				if (values.join(' ').includes(search)) return true;
-				if (search.length < 4) return false;
-				const splitSearch = search.split(' ');
+					if (values.join(' ').includes(search)) return true;
+					if (search.length < 4) return false;
+					const splitSearch = search.split(' ');
 
-				let distanceValues = [];
-				values.forEach((v) => {
-					const words = v.split(' ');
+					let distanceValues = [];
+					values.forEach((v) => {
+						const words = v.split(' ');
 
-					words.forEach((word) => {
-						if (word.length < 4) return;
-						splitSearch.forEach((s) => {
-							const dist = distance(s, word);
-							if (dist < 2) distanceValues.push(dist);
+						words.forEach((word) => {
+							if (word.length < 4) return;
+							splitSearch.forEach((s) => {
+								const dist = distance(s, word);
+								if (dist < 2) distanceValues.push(dist);
+							});
 						});
 					});
-				});
-				// moyenne des distances
-				const averageDistance = distanceValues.reduce((acc, cur) => acc + cur, 0) / distanceValues.length;
-				if (averageDistance < 2) return true;
-				return false;
-			})
+					// moyenne des distances
+					const averageDistance = distanceValues.reduce((acc, cur) => acc + cur, 0) / distanceValues.length;
+					if (averageDistance < 2) return true;
+					return false;
+				})
+				.sort((a, b) => {
+					a.titre.localeCompare(b.titre);
+				})
 		)
 	);
 }
