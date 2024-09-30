4bc47cdaf4230e3ea1bf9c520d3f5d33b3eb8e0b
Kazerlelutin <b@bouteiller.contact>
2024-09-30 22:26:59 +0200

    delete console

--- api/club/by-id.js
+++ api/club/by-id.js
@@ -1,7 +1,6 @@
 import clubs from '../../data/clubs.json';
 
 export async function clubById(_req, id) {
-	console.log('byId', id);
 	const result = clubs.find((club) => club?.id?.toLocaleLowerCase() === id.toLowerCase());
 	const status = result ? 200 : 404;
 	return new Response(JSON.stringify(result || { error: 'Club non trouvé' }, { status }));
